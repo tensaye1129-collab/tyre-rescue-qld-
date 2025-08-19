fe47dab483e9f57d7e5701a882c96007cb828759
tensaye1129-collab <tensaye1129@gmail.com>
2025-08-19 20:55:46 +1000

    Add home screen with service buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
-  runApp(const TyreRescueApp());
-}
+void main() => runApp(const TyreRescueApp());
 
 class TyreRescueApp extends StatelessWidget {
   const TyreRescueApp({super.key});
@@ -13,7 +12,8 @@ class TyreRescueApp extends StatelessWidget {
       title: 'Tyre Rescue QLD',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
       ),
       home: const HomePage(),
     );
@@ -23,35 +23,17 @@ class TyreRescueApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tyre Rescue QLD'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/logo.png',
-              height: 120,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Welcome to Tyre Rescue QLD',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              '24/7 Mobile Tyre, Battery, Fuel & Jump Start Service',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16),
-            ),
-          ],
-        ),
-      ),
-    );
+  // TODO: change to your real number
+  static const String phoneNumber = '0400000000';
+
+  Future<void> _callUs(BuildContext context) async {
+    final uri = Uri(scheme: 'tel', path: phoneNumber);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      _toast(context, 'Could not open dialer');
+    }
   }
-}
+
+  void _toast(BuildContext context, String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(Sn
